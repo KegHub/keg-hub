47e91af02fc22a4d2c368bffdd5ffa57bd73f93f
Lance Tipton <lancetipton04@gmail.com>
2022-01-01 11:59:33 -0700

    fix(core): More config merge updates

--- repos/keg-core/core/scripts/js/loadTapConfig.js
+++ repos/keg-core/core/scripts/js/loadTapConfig.js
@@ -1,5 +1,6 @@
+const path = require('path')
 const corePackConf = require('../../../package.json')
-const { deepMerge, set, get, unset, noOpObj } = require('@keg-hub/jsutils')
+const { deepMerge, set, get, unset, noOpObj, isArr } = require('@keg-hub/jsutils')
 const getAppConfig = require('@keg-hub/tap-resolver/src/resolvers/getAppConfig')
 
 /**
@@ -17,8 +18,11 @@ let loadedConfig
  */
 const getTapPackage = tapPath => {
   /** Try to load the taps package.json, if it fails, just move on */
-  try {return require(tapPath, `package.json`)}
-  catch(err){ return noOpObj}
+  try {return require(path.join(tapPath, `package.json`))}
+  catch(err){
+    console.log(err.message)
+    return noOpObj
+  }
 }
 
 /**
@@ -36,6 +40,7 @@ const updateVersions = (expoConfig, tapConfig, tapPackConf) => {
   expoVer && (expoConfig.sdkVersion = expoVer)
 
   const tapVersion = tapConfig.version || tapPackConf.version
+
   /** Set the expo version to match the merged config version if needed */
   ;tapVersion &&
     // Check the tapConfig to see if it had an expo.version set
@@ -64,6 +69,33 @@ const setupWebConfig = (webConfig, tapConfig, tapPackConf) => {
 }
 
 /**
+ * Ensure configure expo prop array values match the tap.expo values
+ * The deepMerge helper merges arrays instead of overwriting
+ * This method ensures only the taps value is used for array properties
+ * @param {Object} expoConfig - Config object matching expo config spec
+ * @param {Object} tapConfig - Original tap config object before being merged
+ * 
+ * @returns {Object} expoConfig - Config object matching expo config spec
+ */
+const ensureTapArrayValues = (expoConfig, tapConfig) => {
+  const tapExpoConf = tapConfig.expo
+
+  /** If not expo config on the tap config, then nothing to setup */
+  if(!tapExpoConf) return expoConfig
+  
+  /**
+   * Loop the expo config looking for array object
+   * Then replace them with value in the tap config if it exists
+   */
+  Object.entries(expoConfig)
+    .map(([key, value]) => {
+      isArr(value) && (expoConfig[key] = tapExpoConf[key] || expoConfig[key])
+    })
+
+  return expoConfig
+}
+
+/**
  * Sets dynamic values via tap config and package.json
  * @param {Object} mergedConfig - Contains the tap and core configs merged together
  * @param {Object} kegConfig - Config object for keg-core
@@ -85,9 +117,11 @@ const setDynamicValues = (mergedConfig, tapConfig, tapPath) => {
     tapPackConf
   )
 
-  // Ensure the updated configs get set as they might be new objects
+  /**
+   * Ensure the updated configs get set as they might be new objects
+   */
   expoConfig.web = webConfig
-  mergedConfig.expo = expoConfig
+  mergedConfig.expo = ensureTapArrayValues(expoConfig, tapConfig)
 
   return mergedConfig
 }
@@ -103,7 +137,6 @@ const setDynamicValues = (mergedConfig, tapConfig, tapPath) => {
  * @returns {Object} - merged tap and core config Objects
  */
 const loadTapConfig = (kegConfig=noOpObj, tapPath) => {
-
   /** If a cache version exists, then use that */
   if(loadedConfig) return loadedConfig
 
@@ -129,7 +162,8 @@ const loadTapConfig = (kegConfig=noOpObj, tapPath) => {
 
   /**
    * Cache the config so it loads more quickly on subsequent calls
-   * Expo calls this file like 5 times on boot
+   * Expo calls this file a number of times on boot
+   * So this speeds up the process
    */
   loadedConfig = setDynamicValues(mergedConfig, tapConfig, tapPath)
 
